383af315578066e8c469dae9d3e2bed9ee6ee46e
NaN <x@nan.im>
2020-01-13 14:37:36 +0800

    fix ChaIP

--- ChaIP.popclipext/ChaIP.php
+++ ChaIP.popclipext/ChaIP.php
@@ -2,18 +2,25 @@
 
 $text = filter_var(getenv('POPCLIP_TEXT'), FILTER_VALIDATE_IP);
 // $text = '1.2.4.8';
-// if(empty($text)) exit('empty');
-$http = 'http://ip.cn/index.php?ip='.$text;
+$http = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$text;
 
-// get ip info from ip.cn
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($curl, CURLOPT_HEADER, 0);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_URL, $http);
-curl_setopt($curl, CURLOPT_USERAGENT, 'curl/');
-$html = curl_exec($curl);
-$stat = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-curl_close($curl);
+$json = file_get_contents($http);
+// $curl = curl_init();
+// curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+// // curl_setopt($curl, CURLOPT_HEADER, 0);
+// curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($curl, CURLOPT_URL, $http);
+// // curl_setopt($curl, CURLOPT_USERAGENT, 'curl/');
+// $json = curl_exec($curl);
+$info = json_decode($json);
+// $stat = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 // if($stat != '200') exit('');
-echo $html; exit;
+// curl_close($curl);
+
+// if(empty($info->data)) exit('api return null');
+echo $info->data->country,
+	 $info->data->area,
+	 $info->data->city,
+	 $info->data->county,
+	 $info->data->isp;
+exit;
